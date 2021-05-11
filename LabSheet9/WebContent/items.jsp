46eecf06225d2677f87f42d65bdc8f5b09ad48c5
sakuna97 <subi222222@gmail.com>
2021-05-12 02:30:11 +0530

    complete

--- LabSheet9/WebContent/items.jsp
+++ LabSheet9/WebContent/items.jsp
@@ -56,12 +56,16 @@
  		<div class="row">
  			<div class="col">
  				<h1>Items Management</h1>
-				<form method="post" action="items.jsp">
+				<form id="formItem" name="formItem" method="post" action="items.jsp">
 					Item code: <input id="itemCode" name="itemCode" type="text" class="form-control form-control-sm"><br>
 					Item name: <input id="itemName" name="itemName" type="text" class="form-control form-control-sm"><br>
 					Item price: <input id="itemPrice" name="itemPrice" type="text" class="form-control form-control-sm"><br>
 					Item description: <input id="itemDesc" name="itemDesc" type="text" class="form-control form-control-sm"><br>
-					<input id="btnSubmit" name="btnSubmit" type="button" value="save" class="btn btn-primary">
+					
+					<div id="alertSuccess" class="alert alert-success"></div>
+				 	<div id="alertError" class="alert alert-danger"></div>
+				 	
+					<input id="btnSave" name="btnSave" type="button" value="Save" class="btn btn-primary">
 					<input type="hidden" id="hidItemIDSave" name="hidItemIDSave" value="">
 					
 				</form>
--- LabSheet9/src/com/PAF/Item.java
+++ LabSheet9/src/com/PAF/Item.java
@@ -63,50 +63,51 @@ public class Item {
 		 
 		 try{ 
 			 
-		 Connection con = connect(); 
-		 if (con == null){ 
+			 Connection con = connect(); 
 			 
-		 return "Error while connecting to the database for reading."; 
-		 } 
-		 
-		 // Prepare the html table to be displayed
-		 output = "<table border='1'><tr><th>Item Code</th>" 
-		 +"<th>Item Name</th><th>Item Price</th>"
-		 + "<th>Item Description</th>" 
-		 + "<th>Update</th><th>Remove</th></tr>"; 
-		 
-		 String query = "select * from items"; 
-		 Statement stmt = con.createStatement(); 
-		 ResultSet rs = stmt.executeQuery(query); 
-		 
-		 // iterate through the rows in the result set
-		 while (rs.next()){ 
+			 if (con == null){ 
+				 
+			 return "Error while connecting to the database for reading."; 
+			 } 
 			 
-		 String itemID = Integer.toString(rs.getInt("itemID")); 
-		 String itemCode = rs.getString("itemCode"); 
-		 String itemName = rs.getString("itemName"); 
-		 String itemPrice = Double.toString(rs.getDouble("itemPrice")); 
-		 String itemDesc = rs.getString("itemDesc"); 
-		 
-		 // Add a row into the html table
-		 output += "<tr><td><input id='hidItemIDUpdate' name='hidItemIDUpdate' type='hidden' value='" + itemID + "'>" + itemCode + "</td>"; 
-		 output += "<td>" + itemName + "</td>"; 
-		 output += "<td>" + itemPrice + "</td>"; 
-		 output += "<td>" + itemDesc + "</td>";
-		 
-		 // buttons
-		 output += "<td><input name='btnUpdate' " 
-				 + " type='button' value='Update' class='btn btn-dark'></td>"
-				 + "<td><form method='post' action='items.jsp'>"
-				 + "<input name='btnRemove' " 
-				 + " type='submit' value='Remove' class='btn btn-danger'>"
-				 + "<input name='itemID' type='hidden' " 
-				 + " value='" + itemID + "'>" + "</form></td></tr>"; 
-		 } 
-		 con.close(); 
-		 // Complete the html table
-		 output += "</table>"; 
-		 } 
+			 // Prepare the html table to be displayed
+			 output = "<table border='1'><tr><th>Item Code</th>" 
+			 +"<th>Item Name</th><th>Item Price</th>"
+			 + "<th>Item Description</th>" 
+			 + "<th>Update</th><th>Remove</th></tr>"; 
+			 
+			 String query = "select * from items"; 
+			 Statement stmt = con.createStatement(); 
+			 ResultSet rs = stmt.executeQuery(query); 
+			 
+			 // iterate through the rows in the result set
+			 while (rs.next()){ 
+				 
+			 String itemID = Integer.toString(rs.getInt("itemID")); 
+			 String itemCode = rs.getString("itemCode"); 
+			 String itemName = rs.getString("itemName"); 
+			 String itemPrice = Double.toString(rs.getDouble("itemPrice")); 
+			 String itemDesc = rs.getString("itemDesc"); 
+			 
+			 // Add a row into the html table
+			 output += "<tr><td><input id='hidItemIDUpdate' name='hidItemIDUpdate' type='hidden' value='" + itemID + "'>" + itemCode + "</td>"; 
+			 output += "<td>" + itemName + "</td>"; 
+			 output += "<td>" + itemPrice + "</td>"; 
+			 output += "<td>" + itemDesc + "</td>";
+			 
+			 // buttons
+			 output += "<td><input name='btnUpdate' " 
+					 + " type='button' value='Update' class='btnUpdate btn btn-secondary'></td>"
+					 + "<td><form method='post' action='items.jsp'>"
+					 + "<input name='btnRemove' " 
+					 + " type='submit' value='Remove' class='btn btn-danger'>"
+					 + "<input name='hidItemIDDelete' type='hidden' " 
+					 + " value='" + itemID + "'>" + "</form></td></tr>"; 
+			 } 
+			 con.close(); 
+			 // Complete the html table
+			 output += "</table>"; 
+			 } 
 		 
 		catch (Exception e) { 
 			
@@ -139,9 +140,9 @@ public class Item {
 			// execute the statement
 			 preparedStmt.execute(); 
 			 con.close(); 
-			 String newItems = readItems(); 
-			 output = "{\"status\":\"success\", \"data\": \"" + 
-			 newItems + "\"}"; 
+			 ///String newItems = readItems(); 
+			 //output = "{\"status\":\"success\", \"data\": \"" + 
+			 //newItems + "\"}"; 
 			 } 
 			 catch (Exception e) 
 			 { 
